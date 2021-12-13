ffd6655c7581692eac2254ae86df0f4c025818fb
Cory <cory.meikle94@gmail.com>
2021-12-13 21:10:32 +0000

    Added filters to the command, it will now return the data it gets from raider io into discord and show the minimum scores someone can get from the dungeons.

--- commands/MR_Helper/mrHelper.js
+++ commands/MR_Helper/mrHelper.js
@@ -1,11 +1,189 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { default: axios } = require('axios');
+const ascii = require('ascii-table');
+const { MessageEmbed } = require('discord.js');
+
+function parseMessageForArgs(message) {
+    const prefix = '!';
+    const dataToReturn = {
+        error: false,
+        name: '',
+        realm: '',
+        region: 'eu',
+    }
+
+    const args = message.content.trim().split(/ + /g);
+    const cmd = args[0].slice(prefix.length).toLowerCase();
+    const cmdParts = cmd.split(' ');
+    
+    const nameIndex = cmdParts.indexOf('--name');
+    if (nameIndex < 0) {
+        message.channel.send("Please supply a name.");
+
+        dataToReturn.error = true;
+        return dataToReturn;
+    }
+
+    dataToReturn.name = cmdParts[nameIndex + 1];
+
+    const realmIndex = cmdParts.indexOf('--realm');
+    if (realmIndex < 0) {
+        message.channel.send("Please supply a realm.");
+
+        dataToReturn.error = true;
+        return dataToReturn;
+    }
+
+    dataToReturn.realm = cmdParts[realmIndex + 1];
+    
+    return dataToReturn;
+}
+
+function buildRequestUrl(args) {
+    return `https://raider.io/api/v1/characters/profile?region=${args.region}&realm=${args.realm}&name=${args.name}&fields=mythic_plus_best_runs%2Cmythic_plus_alternate_runs`;
+}
+
+function getBlankDataStructure() {
+    return {
+        'SD': {
+            'fortified': {},
+            'tyrannical': {},
+        },
+        'SOA': {
+            'fortified': {},
+            'tyrannical': {},
+        },
+        'MISTS': {
+            'fortified': {},
+            'tyrannical': {},
+        },
+        'PF': {
+            'fortified': {},
+            'tyrannical': {},
+        },
+        'TOP': {
+            'fortified': {},
+            'tyrannical': {},
+        },
+        'NW': {
+            'fortified': {},
+            'tyrannical': {},
+        },
+        'DOS': {
+            'fortified': {},
+            'tyrannical': {},
+        },
+        'HOA': {
+            'fortified': {},
+            'tyrannical': {},
+        },
+    };
+}
+
+function calculateScores(isFortifiedBest, dungeon) {
+    let target;
+    if (isFortifiedBest) {
+        target = 'fortified';
+    } else {
+        target = 'tyrannical';
+    }
+
+    const bestRunScore = dungeon[target].score * 1.5;
+    let altRunScore = 0;
+    const otherDungeonAffix = target === 'fortified' ? 'tyrannical' : 'fortified';
+
+    if (dungeon[otherDungeonAffix].score) {
+        altRunScore = dungeon[otherDungeonAffix].score / 2;
+    }
+
+    maxAltRun = (bestRunScore / 3) - altRunScore;
+
+    return {
+        potentialScore: maxAltRun,
+        dungeonLongName: dungeon[target].dungeon,
+        affix: target,
+        totalScore: bestRunScore + altRunScore,
+    }
+}
+
+async function requestAndFormatData(args) {
+    const url = buildRequestUrl(args);
+    const res = await axios({
+        method: 'get',
+        url: url,
+    });
+
+    const allDungeons = getBlankDataStructure();
+
+    // best runs
+    for (const dungeon of res.data.mythic_plus_best_runs) {
+        dungeon.isBestRun = true;
+        const isFortified = dungeon.affixes[0].id === 10;
+        const allDungeonsTarget = isFortified ? 'fortified' : 'tyrannical';
+
+        allDungeons[dungeon.short_name][allDungeonsTarget] = dungeon;
+    }
+
+    // alternate runs
+    for (const dungeon of res.data.mythic_plus_alternate_runs) {
+        dungeon.isBestRun = false;
+        const isFortified = dungeon.affixes[0].id === 10;
+        const allDungeonsTarget = isFortified ? 'fortified' : 'tyrannical';
+
+        allDungeons[dungeon.short_name][allDungeonsTarget] = dungeon;
+    }
+
+    let totalScore = 0;
+    let pointsFromAltRuns = 0;
+    const dataToReturn = [];
+    for (const dungeonName of Object.keys(allDungeons)) {
+        const dungeon = allDungeons[dungeonName];
+        const isFortifiedBest = dungeon.fortified.isBestRun;
+        const scores = calculateScores(isFortifiedBest, dungeon);
+
+        totalScore += scores.totalScore;
+        pointsFromAltRuns += scores.potentialScore;
+        dataToReturn.push(scores);
+    }
+
+    console.log('---------------------------------------------------------------------------------------------');
+    console.log(`Current score for ${res.data.name}: ${totalScore}`);
+    console.log(`The max points you can earn from improving your alt runs are: ${pointsFromAltRuns}`);
+    console.log('---------------------------------------------------------------------------------------------');
+
+    return dataToReturn;
+}
+
+function jsonDataToAsciiTable(json) {
+    const table = new ascii().setHeading("Dungeon", "Affix", "More info");
+
+    for (const dungeon of json) {
+
+        table.addRow(
+            dungeon.dungeonLongName,
+            dungeon.affix,
+            `You can earn a minimum of ${Math.ceil(dungeon.potentialScore)} points by running this dungeon.`
+        );
+    }
+
+    return table.toString();
+}
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('mr-helper')
         .setDescription('Get dungeons to run to improve overall mythic rating'),
-    async execute(interaction) {
+    async execute(interaction, message) {
+        const args = parseMessageForArgs(message);
 
-        await interaction.reply('test 1234');
+        if (args.error) {
+            return;
+        }
+
+        const dungeonData = await requestAndFormatData(args);
+        
+        const dataToSend = jsonDataToAsciiTable(dungeonData);
+
+        await interaction.reply(dataToSend);
     },
 }
--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 const { loadCommands } = require('./handler/loadCommands');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ 
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+    ] 
+});
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -11,6 +16,23 @@ client.once('ready', () => {
 client.commands = new Collection();
 
 loadCommands(client);
+
+const prefix = '!';
+client.on('message', async message => {   
+
+    const args = message.content.trim().split(/ + /g);
+    const cmd = args[0].slice(prefix.length).toLowerCase();
+    const cmdParts = cmd.split(' ');
+    const cmdName = cmdParts[0];
+
+    const command = client.commands.get(cmdName);
+
+    if (!command) {
+        return;
+    }
+
+    command.execute(message, message);
+});
 
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) {

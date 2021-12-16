6443718b076fbf90cdb4964a15d2b6280afab4c5
Cory <cory.meikle94@gmail.com>
2021-12-16 21:09:19 +0000

    Move functions to ES6 syntax in functions.js. Added a new function to send an embedded message. Added a new info command. More eslint fixes

--- reusables/functions.js
+++ reusables/functions.js
@@ -1,13 +1,14 @@
 const ascii = require('ascii-table');
+const { MessageEmbed } = require('discord.js');
 
 const BASE_SCORE_PER_LEVEL = 7.5;
 
-function getBaseScoreForKeystoneLevel(keystoneLevel) {
+const getBaseScoreForKeystoneLevel = (keystoneLevel) => {
 
     return BASE_SCORE_PER_LEVEL * keystoneLevel;
-}
+};
 
-function getBaseScoreForAffix(affix) {
+const getBaseScoreForAffix = (affix) => {
     const seasonalAffix = 'tormented';
 
     if (affix === seasonalAffix) {
@@ -15,21 +16,36 @@ function getBaseScoreForAffix(affix) {
     }
 
     return BASE_SCORE_PER_LEVEL;
-}
+};
 
-function buildTableFromJson(jsonData) {
+const buildTableFromJson = (jsonData) => {
     const table = new ascii();
 
     return table.fromJSON(jsonData);
-}
+};
 
-async function sendStructuredResponseToUser(interaction, response) {
+const sendStructuredResponseToUser = async (interaction, response) => {
     await interaction.reply('```' + response + '```');
-}
+};
+
+const sendEmbeddedMessage = (discordMessage, messageObj) => {
+    const embedMessage = new MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle(messageObj.title)
+        .setAuthor(messageObj.author.name, messageObj.author.img, messageObj.author.link)
+        .setDescription(messageObj.description)
+        .addFields(messageObj.fields)
+        .setTimestamp();
+
+    discordMessage.channel.send({
+        embeds: [embedMessage]
+    });
+};
 
 module.exports = {
     getBaseScoreForAffix: getBaseScoreForAffix,
     getBaseScoreForKeystoneLevel: getBaseScoreForKeystoneLevel,
     buildTableFromJson: buildTableFromJson,
-    sendStructuredResponseToUser: sendStructuredResponseToUser
+    sendStructuredResponseToUser: sendStructuredResponseToUser,
+    sendEmbeddedMessage: sendEmbeddedMessage,
 };

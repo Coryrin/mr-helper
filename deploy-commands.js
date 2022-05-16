c92208f9a41f12ec3323fae0382a23f586303442
Cory Meikle <cory.meikle94@gmail.com>
2022-05-16 12:46:26 +0100

    Update deploy commands to work for debug/prod

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,10 +4,9 @@ const { Routes } = require('discord-api-types/v9');
 require('dotenv').config();
 
 const commands = [];
-
 const commandFolders = fs.readdirSync('./commands');
-for (const folder of commandFolders) {
 
+for (const folder of commandFolders) {
     const commandFiles = fs
         .readdirSync(`./commands/${folder}`)
         .filter((file) => file.endsWith('.js'));
@@ -21,6 +20,13 @@ for (const folder of commandFolders) {
 
 const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 
-rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+if (process.env.DEBUG) {
+    rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
+        .then(() => console.log('Successfully registered application commands.'))
+        .catch(console.error);
+} else {
+    rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands })
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch(console.error);
+}
+
